fcafb01b0efee0f9850cbe9eea7082f61327d851
lynx-baudilio <devlynx06baudilio@gmail.com>
2026-08-23 18:29:12 -0600

    feat: generar la estructura base del proyecto con maven y la clase Main

---
+++ 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package usac.cunoc.ipc1.poketerminal;
+
+/**
+ *
+ * @author baudiliorr
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
