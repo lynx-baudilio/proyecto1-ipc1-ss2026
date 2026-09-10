a2f51ee776ee1e3704426e23b733949997851736
lynx-baudilio <devlynx06baudilio@gmail.com>
2026-09-09 21:48:41 -0600

    feat: crear la clase Jugador y la clase Posicion

---
+++ 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/modelos/Jugador.java
@@ -0,0 +1,40 @@
+package usac.cunoc.ipc1.poketerminal.modelos;
+
+import java.io.Serializable;
+
+public class Jugador implements Serializable {
+    
+    private static final int POKEMONES_MAX = 6;
+    private String nombreJugador;
+    private int pokemonesActuales;
+    private Posicion posicion;
+    
+    public Jugador(String nombreJugador, Posicion posicion) {
+        this.nombreJugador = nombreJugador;
+        this.posicion = posicion;
+        this.pokemonesActuales = 1;
+    }
+    
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public int getPokemonesActuales() {
+        return pokemonesActuales;
+    }
+
+    public void setPokemonesActuales(int pokemonesActuales) {
+        this.pokemonesActuales = pokemonesActuales;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+    
+    
+    
+}
---
+++ 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/modelos/Posicion.java
@@ -0,0 +1,31 @@
+package usac.cunoc.ipc1.poketerminal.modelos;
+
+public class Posicion {
+    
+    private int fila;
+    private int columna;
+    
+    public Posicion(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+    
+    
+    
+}
