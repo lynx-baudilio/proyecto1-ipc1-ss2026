0d047ab1a90259017281308da0b987f135431036
lynx-baudilio <devlynx06baudilio@gmail.com>
2026-09-14 22:09:18 -0600

    feat: cambiar la pantalla al pedir el nombre de partida, generar las instancias necesarias para una nueva partida, implementar el gestor de archivos para guardar la partida, crear el esqueleto base del movimiento del jugador en el mapa

--- 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/PoketerminalApp.java
+++ 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/PoketerminalApp.java
@@ -1,6 +1,5 @@
 package usac.cunoc.ipc1.poketerminal;
 
-import java.util.Scanner;
 import usac.cunoc.ipc1.poketerminal.catalogos.CatalogoPokemones;
 import usac.cunoc.ipc1.poketerminal.modelos.Jugador;
 import usac.cunoc.ipc1.poketerminal.modelos.Partida;
@@ -10,11 +9,11 @@ import usac.cunoc.ipc1.poketerminal.ui.Ansi;
 import usac.cunoc.ipc1.poketerminal.ui.AsciiArt;
 import usac.cunoc.ipc1.poketerminal.ui.DialogoNPC;
 import usac.cunoc.ipc1.poketerminal.ui.PixelArt;
+import usac.cunoc.ipc1.poketerminal.utilidades.GestorDeArchivos;
 import usac.cunoc.ipc1.poketerminal.utilidades.LeerEntrada;
 
 public class PoketerminalApp {
     
-    private final Scanner scanner = new Scanner(System.in);
     private final LeerEntrada leerEntrada = new LeerEntrada();
     private final Ansi ansi = new Ansi();
     private final AsciiArt asciiArt = new AsciiArt();
@@ -37,7 +36,7 @@ public class PoketerminalApp {
         System.out.println(asciiArt.getTEXTO_TITULO());
         System.out.println(pixelArt.getPORTADA());
         System.out.println(asciiArt.getBORDE_INFERIOR());
-        scanner.nextLine();
+        leerEntrada.enter();
     }
 
     private void mostrarMenu() {
@@ -69,7 +68,7 @@ public class PoketerminalApp {
                     break;
                 default:
                     System.out.println("Tecla inválida. Presiona ENTER para volver a intentar");
-                    scanner.nextLine();
+                    leerEntrada.enter();
             }
 
         } while (programaActivo);
@@ -78,13 +77,24 @@ public class PoketerminalApp {
     private void ejecutarOpcionSeleccionada() {
         switch (opcionSeleccionada) {
             case 1:
-                iniciarNuevaPartida();
-                scanner.nextLine();
+                Partida partidaNueva = iniciarNuevaPartida();
+                explorarMapa(partidaNueva);
+                leerEntrada.enter();
                 break;
             case 2:
-                System.out.println("Cargando partida");
-                scanner.nextLine();
-                break;
+                String nombrePartida = leerEntrada.texto("Ingresa el nombre de tu partida:");
+                GestorDeArchivos gestor = new GestorDeArchivos();
+
+                Partida partidaCargada = gestor.cargarPartida(nombrePartida);
+
+                if (partidaCargada != null) {
+                    System.out.println("Partida cargada de: " + partidaCargada.getJugador().getNombreJugador());
+                    explorarMapa(partidaCargada);
+                } else {
+                    System.out.println("No existe esa partida manin.");
+                }
+                leerEntrada.enter();
+                break;            
             case 3:
                 System.out.println("Cerrando programa");
                 programaActivo = false;
@@ -92,7 +102,7 @@ public class PoketerminalApp {
         }
     }
     
-    private void iniciarNuevaPartida() {
+    private Partida iniciarNuevaPartida() {
         ansi.limpiarPantalla();
         
         String nombrePartida = pedirNombreDePartida();
@@ -109,7 +119,7 @@ public class PoketerminalApp {
         mostrarDialogo.profesorOak("Pero pues si, empecemos por lo principal, ¿Cuál es tu nombre?");
         String nombreJugador = leerEntrada.texto("Ingresa tu nombre de entrenador");
         
-        mostrarDialogo.profesorOak("¡Que calidad conocerte " + nombreJugador + ":D!");
+        mostrarDialogo.profesorOak("¡Que calidad conocerte " + nombreJugador + " :D!");
         leerEntrada.enter();
         mostrarDialogo.profesorOak("Ahora debes eligir tu pokemon inicial.");
         leerEntrada.enter();
@@ -137,17 +147,59 @@ public class PoketerminalApp {
         
         Posicion posicion = new Posicion(2, 2);
         Jugador jugador = new Jugador(nombreJugador, posicion, pokemonInicial);
-        System.out.println("Se supone que todo bien :p");
-        scanner.nextLine();
-        
-        ansi.limpiarPantalla();
         Partida partidaActual = new Partida(nombrePartida, jugador);
         
-        mostrarDialogo.profesorOak("¡Nítido " + nombreJugador + "! Ahora te entrego a tu pokemon y suerte :D");
+        mostrarDialogo.profesorOak("¡Nítido mi estimado " + partidaActual.getJugador().getNombreJugador() + "! Ahora te entrego a tu pokemon y suerte :D");
         leerEntrada.enter();
         
-        ansi.limpiarPantalla();
-        partidaActual.getCiudadActual().imprimirMapa();
+        return partidaActual;
+    }
+    
+    private void explorarMapa(Partida partidaActual) {
+        boolean explorando = true;
+        GestorDeArchivos gestor = new GestorDeArchivos();
+
+        do {
+            ansi.limpiarPantalla();
+
+            System.out.println(Ansi.AMARILLO + "Entrenador: " + partidaActual.getJugador().getNombreJugador()
+                    + " | Ciudad: " + partidaActual.getCiudadActual().getNombreCiudad() + Ansi.RESET);
+
+            partidaActual.getCiudadActual().imprimirMapa();
+
+            String entrada = leerEntrada.texto(
+                    "Controles: [W/A/S/D] Moverte | [G] Guardar Partida | [X] Salir al Menú Principal"
+            ).toLowerCase().trim();
+
+            switch (entrada) {
+                case "w":
+                    // Lógica para subir fila en la Posicion del jugador
+                    break;
+                case "s":
+                    // Lógica para bajar fila
+                    break;
+                case "a":
+                    // Lógica para mover columna a la izquierda
+                    break;
+                case "d":
+                    // Lógica para mover columna a la derecha
+                    break;
+                case "g":
+                    // Guardar la partida inmediatamente en archivo .dat
+                    if (gestor.guardarPartida(partidaActual)) {
+                        System.out.println(Ansi.VERDE + "¡Partida guardada exitosamente!" + Ansi.RESET);
+                    }
+                    leerEntrada.enter();
+                    break;
+                case "x":
+                    explorando = false;
+                    break;
+                default:
+                    System.out.println("Tecla no válida.");
+                    break;
+            }
+
+        } while (explorando);
     }
     
     private void eleccionPokemon() {        
@@ -175,7 +227,7 @@ public class PoketerminalApp {
                 return;
             default:
                 System.out.println("Entrada inválida. Presiona ENTER para volver a intentar");
-                scanner.nextLine();        
+                leerEntrada.enter();
         }
     }
     
@@ -192,7 +244,7 @@ public class PoketerminalApp {
     }
     
     private String pedirNombreDePartida() {
-        System.out.println(asciiArt.getINGRESAR_NOMBRE_PARTIDA());
+        System.out.println(pixelArt.getPEDIR_NOMBRE_DE_PARTIDA());
         return leerEntrada.texto("Escriba un nombre para identificar tu partida :p");
     }
 }
