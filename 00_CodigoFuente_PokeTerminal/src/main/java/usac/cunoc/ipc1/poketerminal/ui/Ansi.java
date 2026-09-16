5d0085ec2eb6fbd296f3d5e8c3028afc7227f7a2
lynx-baudilio <devlynx06baudilio@gmail.com>
2026-09-15 19:21:18 -0600

    feat: agregar color negro en codigo Ansi

--- 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/ui/Ansi.java
+++ 00_CodigoFuente_PokeTerminal/src/main/java/usac/cunoc/ipc1/poketerminal/ui/Ansi.java
@@ -4,6 +4,7 @@ public class Ansi {
     
     public static final String RESET = "\u001B[0m";
     public static final String NEGRITA = "\u001B[1m";
+    public static final String NEGRO = "\u001B[30m";
     public static final String ROJO = "\u001B[31m";
     public static final String VERDE = "\u001B[32m";
     public static final String AMARILLO = "\u001B[33m";
